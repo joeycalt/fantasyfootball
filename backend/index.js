3bc794a26b70dd4dd4d5fcbef062fe35a6ff23f3
joeycalt <joeycalt@yahoo.com>
2022-10-24 15:39:36 -0400

    added controllers and index content

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const PORT = 4000;
+require('dotenv').config();
+
+require('./config/db.connection')
+
+const express = require ('express')
+
+const morgan = require('morgan')
+
+const cors = require('cors')
+
+const PORT = process.env.PORT || 4000
+const factsController = require('./controllers/facts-controller')
+
+
+const app = express()
+
+app.use(express.json());
+
+app.use(morgan('dev'));
+
 app.use(cors());
-app.listen(PORT, function() {
-  console.log("Server is running on Port: " + PORT);
-});
+
+app.use('/food', factsController)
+
+app.get('/',(req,res) => {
+res.send('Hello World')
+
+})
+
+app.listen(PORT, ()=> console.log(`listening on ${PORT} ✅`))
